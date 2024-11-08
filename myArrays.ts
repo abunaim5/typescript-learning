f472a41686a26f88e1e6da8b7db56f87c1b74fd4
Abu Naim <abunaim140@gmail.com>
2024-11-09 00:53:14 +0600

    learned array types in typescript

---
+++ myArrays.ts
@@ -0,0 +1,11 @@
+const userNames: string[] = []
+// const userIds: number[] = []
+const userIds: Array<number> = []
+
+const colorCodes: string[][] = [
+    ['255', '255', '255'],
+    []
+]
+
+userNames.push('naim')
+userIds.push(5)
